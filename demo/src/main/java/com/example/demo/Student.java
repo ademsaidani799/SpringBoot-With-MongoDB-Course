27aebb48f82934dc47037de8b0b3691971a7861f
adem saidani <ademsaidani799@gmail.com>
2023-04-07 05:45:32 +0200

    my first commit

---
+++ demo/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+ @Data
+ @Document
+public class Student{
+     @Id
+     private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Gender gender;
+    private Address adress;
+    private List<String> favouriteSubjects;
+
+    private BigDecimal totalSpentInBooks;
+    private ZonedDateTime created;
+
+
+}
